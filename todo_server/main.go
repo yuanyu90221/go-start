d26998d829d4f5128002f1ae6df844a26629d21f
Json <yuanyu90221@gmail.com>
2019-12-22 00:55:40 +0800

    update

--- todo_server/main.go
+++ todo_server/main.go
@@ -1,12 +1,20 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/yuanyu90221/go-start/todo_server/structs"
+)
 
 func main() {
 	mux := http.NewServeMux()
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
-			// fmt.Pirnt("test"
+			data := structs.Response{
+				Code: http.StatusOK
+				Body: "pong",
+			}
+			json.NewEncoder(w).Encode(data)
 		}
 	})
 	http.ListenAndServe(":3000", nil)
--- todo_server/structs/struct.go
+++ todo_server/structs/struct.go
@@ -2,5 +2,5 @@ package structs
 
 type Response struct {
 	Code int         `json: "code"`
-	Body interface{} `json:"bodu"`
+	Body interface{} `json:"body"`
 }
