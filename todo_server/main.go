b1d435f81af4055edf7c44ebf564bad3a201a483
Json <yuanyu90221@gmail.com>
2019-12-22 00:59:45 +0800

    update

--- todo_server/main.go
+++ todo_server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"net/http"
 
 	"github.com/yuanyu90221/go-start/todo_server/structs"
@@ -11,11 +12,11 @@ func main() {
 	mux.HandleFunc("/ping", func(w http.ResponseWriter, r *http.Request) {
 		if r.Method == http.MethodGet {
 			data := structs.Response{
-				Code: http.StatusOK
+				Code: http.StatusOK,
 				Body: "pong",
 			}
 			json.NewEncoder(w).Encode(data)
 		}
 	})
-	http.ListenAndServe(":3000", nil)
+	http.ListenAndServe(":3000", mux)
 }
